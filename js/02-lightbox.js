536ca3720bb0309b70cef94e3c2fbe96eb22d0ed
UserVasyk <106909139+UserVasyk@users.noreply.github.com>
2022-10-27 10:45:32 +0300

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector(".gallery");
+
+function makingGalleryItem(options) {
+  return options
+    .map(
+      (item) =>
+        `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+    )
+    .join("");
+}
+galleryBox.insertAdjacentHTML("beforeend", makingGalleryItem(galleryItems));
+
+new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
